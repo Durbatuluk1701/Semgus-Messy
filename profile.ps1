81116cdeb501256c0c2c6194a4f68f6f3e0652da
Will Thomas <30wthomas@gmail.com>
2023-11-30 10:10:49 -0600

    Updating profiling tools

--- profile.ps1
+++ profile.ps1
@@ -6,28 +6,24 @@ $numTests = 5
 
 foreach ($file in $testFiles) {
   # Write-Host "Times Taken for ${file}:"
-  Write-Host -NoNewline "${file}"
+  # Write-Host -NoNewline "${file}"
   $messyTimeTotal = 0
   for($i=1; $i -le $numTests; $i++){
     $messyTime = Measure-Command {
-      scala ./Messy.jar -i "./tests/$file" 2> $null
+      scala ./Messy.jar -i "./tests/$file"
       z3 out.z3
     }
     $messyTimeTotal+= $messyTime.TotalSeconds
   }
-  Write-Host -NoNewline ", $($messyTimeTotal/$numTests)"
+  Write-Host -NoNewline "$($messyTimeTotal/$numTests)"
   $ntTimeTotal = 0
   for($i=1; $i -le $numTests; $i++){
     $ntTime = Measure-Command {
-      scala .\target\scala-2.13\Messy-1.0-fat.jar -i "./tests/$file" --nt 2> $null
+      scala .\target\scala-2.13\Messy-1.0-fat.jar -i "./tests/$file" --nt
     }
     $ntTimeTotal += $ntTime.TotalSeconds
   }
   Write-Host -NoNewline ", $($ntTimeTotal/$numTests)"
   Write-Host ""
-    # Write-Host ", $($messyTime.TotalSeconds)"
-  # Write-Host "${file}, $($messyTime.TotalSeconds), $($ntTime.TotalSeconds)"
-  # $prodTime = Measure-Command {scala .\target\scala-2.13\Messy-1.0-fat.jar -i "./tests/$file" --prod}
-  # Write-Host "`tProd: $($prodTime.TotalSeconds)"
 }
 
