c265f62fa017e78f572237c81e3087b71ec8063d
Will Thomas <30wthomas@gmail.com>
2023-11-30 09:10:26 -0600

    Updating test suite to get rid of broken tests

--- profile.ps1
+++ profile.ps1
@@ -2,17 +2,28 @@ $testFiles = Get-ChildItem -Path "./tests" -File
   
 Write-Host "Test, Messy Time (s), NT Time (s)`n"
 
+$numTests = 5
+
 foreach ($file in $testFiles) {
   # Write-Host "Times Taken for ${file}:"
   Write-Host -NoNewline "${file}"
-  for($i=1; $i -le 5; $i++){
-    $messyTime = Measure-Command {scala ./Messy.jar -i "./tests/$file" 2> $null}
-    Write-Host -NoNewline ", $($messyTime.TotalSeconds)"
+  $messyTimeTotal = 0
+  for($i=1; $i -le $numTests; $i++){
+    $messyTime = Measure-Command {
+      scala ./Messy.jar -i "./tests/$file" 2> $null
+      z3 out.z3
+    }
+    $messyTimeTotal+= $messyTime.TotalSeconds
   }
-  for($i=1; $i -le 5; $i++){
-    $ntTime = Measure-Command {scala .\target\scala-2.13\Messy-1.0-fat.jar -i "./tests/$file" --nt 2> $null}
-    Write-Host -NoNewline ", $($ntTime.TotalSeconds)"
+  Write-Host -NoNewline ", $($messyTimeTotal/$numTests)"
+  $ntTimeTotal = 0
+  for($i=1; $i -le $numTests; $i++){
+    $ntTime = Measure-Command {
+      scala .\target\scala-2.13\Messy-1.0-fat.jar -i "./tests/$file" --nt 2> $null
+    }
+    $ntTimeTotal += $ntTime.TotalSeconds
   }
+  Write-Host -NoNewline ", $($ntTimeTotal/$numTests)"
   Write-Host ""
     # Write-Host ", $($messyTime.TotalSeconds)"
   # Write-Host "${file}, $($messyTime.TotalSeconds), $($ntTime.TotalSeconds)"
