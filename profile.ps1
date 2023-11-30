7f3d2d61f13a625c4b73e24076c3f8e52a9f5bf1
Will Thomas <30wthomas@gmail.com>
2023-11-29 23:57:16 -0600

    Profiling updates

--- profile.ps1
+++ profile.ps1
@@ -1,11 +1,21 @@
 $testFiles = Get-ChildItem -Path "./tests" -File
+  
+Write-Host "Test, Messy Time (s), NT Time (s)`n"
 
 foreach ($file in $testFiles) {
-  Write-Host "Times Taken for ${file}:"
-  $messyTime = Measure-Command {scala ./Messy.jar -i "./tests/$file"}
-  Write-Host "`tMessy: $($messyTime.TotalSeconds)"
-  $ntTime = Measure-Command {scala .\target\scala-2.13\Messy-1.0-fat.jar -i "./tests/$file" --nt}
-  Write-Host "`tNT: $($ntTime.TotalSeconds)"
+  # Write-Host "Times Taken for ${file}:"
+  Write-Host -NoNewline "${file}"
+  for($i=1; $i -le 5; $i++){
+    $messyTime = Measure-Command {scala ./Messy.jar -i "./tests/$file" 2> $null}
+    Write-Host -NoNewline ", $($messyTime.TotalSeconds)"
+  }
+  for($i=1; $i -le 5; $i++){
+    $ntTime = Measure-Command {scala .\target\scala-2.13\Messy-1.0-fat.jar -i "./tests/$file" --nt 2> $null}
+    Write-Host -NoNewline ", $($ntTime.TotalSeconds)"
+  }
+  Write-Host ""
+    # Write-Host ", $($messyTime.TotalSeconds)"
+  # Write-Host "${file}, $($messyTime.TotalSeconds), $($ntTime.TotalSeconds)"
   # $prodTime = Measure-Command {scala .\target\scala-2.13\Messy-1.0-fat.jar -i "./tests/$file" --prod}
   # Write-Host "`tProd: $($prodTime.TotalSeconds)"
 }
